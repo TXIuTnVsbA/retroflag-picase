67bdafc7eea32f2b1886562e2ca6feb7f4bd0572
TXIuTnVsbA <qqsmallyu80@gmail.com>
2022-03-13 22:26:04 +0800

    Update SafeShutdown_gpi.py

--- SafeShutdown_gpi.py
+++ SafeShutdown_gpi.py
@@ -11,9 +11,6 @@ power.on()
 
 #functions that handle button events
 def when_pressed():
-#  os.system("sudo killall emulationstation")
-#  os.system("sudo killall emulationstatio") #RetroPie 4.6
-#  os.system("sleep 5s")
   os.system("sudo shutdown -h now")
   
 btn = Button(powerPin, hold_time=hold)

4ac9f86da84dc69a4a4fae35dd9fe2e08dee4786
TXIuTnVsbA <qqsmallyu80@gmail.com>
2022-03-13 22:25:12 +0800

    Update SafeShutdown.py

--- SafeShutdown.py
+++ SafeShutdown.py
@@ -25,10 +25,10 @@ def poweroff():
 	while True:
 		#self.assertEqual(GPIO.input(powerPin), GPIO.LOW)
 		GPIO.wait_for_edge(powerPin, GPIO.FALLING)
-		os.system("sudo killall emulationstation")
-		os.system("sudo killall emulationstatio") #RetroPie 4.6
-		os.system("sudo sleep 5s")
-		os.system("sudo shutdown -r now")
+#		os.system("sudo killall emulationstation")
+#		os.system("sudo killall emulationstatio") #RetroPie 4.6
+#		os.system("sudo sleep 5s")
+		os.system("sudo shutdown -h now")
 
 #blinks the LED to signal button being pushed
 def ledBlink():
@@ -48,10 +48,10 @@ def reset():
 	while True:
 		#self.assertEqual(GPIO.input(resetPin), GPIO.LOW)
 		GPIO.wait_for_edge(resetPin, GPIO.FALLING)
-		os.system("sudo killall emulationstation")
-		os.system("sudo killall emulationstatio") #RetroPie 4.6
-		os.system("sudo sleep 5s")
-		os.system("sudo shutdown -r now")
+#		os.system("sudo killall emulationstation")
+#		os.system("sudo killall emulationstatio") #RetroPie 4.6
+#		os.system("sudo sleep 5s")
+		os.system("sudo shutdown -h now")
 
 
 if __name__ == "__main__":

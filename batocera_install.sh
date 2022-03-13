269cc6d78889cae84582f8847fdba092badf3a9d
TXIuTnVsbA <qqsmallyu80@gmail.com>
2022-03-13 22:20:25 +0800

    Update batocera_install.sh

--- batocera_install.sh
+++ batocera_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SourcePath=https://raw.githubusercontent.com/RetroFlag/retroflag-picase/master
+SourcePath=https://raw.githubusercontent.com/TXIuTnVsbA/retroflag-picase/master
 
 #-----------------------------------------------------------
 sleep 2s
